2e3f8c1e74dca97b50259102fad1f73d88427860
Madi Baig <madi.baig@protonmail.com>
2022-02-09 21:21:49 +0000

    Added forced execution of for loop bodies

---
+++ safe_script_examples/multi-exec/for_in_test_2.js
@@ -0,0 +1,5 @@
+var x = [];
+
+for (var i in x) {
+  console.log("EXECUTED BODY");
+}
---
+++ safe_script_examples/multi-exec/for_test.js
@@ -0,0 +1,7 @@
+for (var i = 0; i < -1; i++) {
+  console.log("First test passed");
+}
+
+for (var i = 0; i < 10; i++) {
+  console.log("FOR body loop " + i);
+}
