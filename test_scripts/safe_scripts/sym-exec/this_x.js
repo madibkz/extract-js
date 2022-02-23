6d6d34d832f3bc731c38f1e2d7f26777e8f94354
Madi Baig <madi.baig@protonmail.com>
2022-02-23 00:28:30 +0000

    Implemented symbolic variables being globally scoped for greater coverage

---
+++ test_scripts/safe_scripts/sym-exec/this_x.js
@@ -0,0 +1,21 @@
+//console.log("x is " + this.x);
+//for (var i in this) {
+  //console.log("I is " + i);
+//}
+
+console.log("x is " + x);
+
+//throw `${this}`;
+//if (this) {
+//  throw "THIS";
+//}
+//if (globalThis) { //works
+  //throw "GLOBAL THIS";
+//}
+///if (window) { // doesnt work
+  //throw "WINDOW";
+//}
+//if (global) { // works
+//  throw "GLOBAL OBJECT";
+//}
+//throw "NO THIS";
