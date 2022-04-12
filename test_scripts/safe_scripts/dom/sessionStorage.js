828fe39e6d2854a1fd8190178a871030ee38b1d4
Madi Baig <madi.baig@protonmail.com>
2022-04-12 17:58:53 +0100

    Added saving of localStorage and sessionStorage to json files and aggregation of them

--- test_scripts/safe_scripts/dom/sessionStorage.js
+++ test_scripts/safe_scripts/dom/sessionStorage.js
@@ -4,4 +4,4 @@ sessionStorage.setItem("key1", "value1");
 console.log(sessionStorage.getItem("key1"));
 sessionStorage.removeItem("key1");
 sessionStorage.setItem("key2", "value2");
-sessionStorage.clear();
+//sessionStorage.clear();
