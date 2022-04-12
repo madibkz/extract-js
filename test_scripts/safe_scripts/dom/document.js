6dd7345317aa2d7f21787d5ba860d7cb5c8e353e
Madi Baig <madi.baig@protonmail.com>
2022-04-12 16:38:39 +0100

    Added logging of location when called through document and fixed cookie document proxy error

--- test_scripts/safe_scripts/dom/document.js
+++ test_scripts/safe_scripts/dom/document.js
@@ -2,3 +2,6 @@ console.log(document.referrer);
 document.cookie = "set=value;";
 console.log(document.cookie);
 console.log(document.createElement("div"));
+
+console.log(document.location);
+console.log(document.location.href);
--- test_scripts/safe_scripts/dom/location_test.js
+++ test_scripts/safe_scripts/dom/location_test.js
@@ -1,7 +1,8 @@
 //console.log("awfawf");
-//console.log("1" + location);
-//console.log("2" + location.href);
-console.log("3" + document.location.href);
+console.log("1" + location);
+console.log("2" + location.href);
+console.log("3" + document.location);
+console.log("4" + document.location.href);
 //
 //location.host = "awfawf";
 //location = "https://google.com";
